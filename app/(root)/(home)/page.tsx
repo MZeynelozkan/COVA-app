40a5b44697dc3eea72389487c12d5fd50571a85b
MZeynelozkan <zeyzey4340@gmail.com>
2024-12-03 15:00:13 +0300

    update

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -4,7 +4,7 @@ import ItemCard from "@/components/cards/ItemCard";
 
 const Home = () => {
   return (
-    <div className="my-5 grid flex-1 grid-cols-3 place-items-center gap-x-8 p-4 max-2xl:grid-cols-2 max-md:gap-y-5 max-sm:grid-cols-2 max-[375px]:grid-cols-1">
+    <div className="my-5 grid flex-1 grid-cols-3 place-items-center gap-x-8 p-4 max-2xl:grid-cols-2 max-md:gap-y-5  max-sm:grid-cols-2 max-[375px]:grid-cols-1">
       {Array.from({ length: 3 }).map((item, index) => (
         <ItemCard key={index} />
       ))}
