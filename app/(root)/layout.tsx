9e3e59b4db6c27e2b2fc8af58aa4be275ebab0c2
Zeynel <117638205+MZeynelozkan@users.noreply.github.com>
2024-12-03 17:41:20 +0300

    Update layout.tsx

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -13,7 +13,7 @@ const Layout = ({ children }: Props) => {
       <Navbar />
       <div className="flex  ">
         <LeftSidebar />
-        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
+        <section className="flex min-h-screen flex-1 flex-col px-6 pb-5 pt-24">
           {children}
         </section>
       </div>
