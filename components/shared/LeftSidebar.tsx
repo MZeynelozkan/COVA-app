0df21f73bd73c64ef51bfd25a17b323e4bd34251
Zeynel <117638205+MZeynelozkan@users.noreply.github.com>
2024-12-03 17:38:25 +0300

    Update LeftSidebar.tsx

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -23,7 +23,7 @@ const LeftSidebar = () => {
   });
 
   return (
-    <div className="sticky top-0 flex h-screen w-full max-w-[320px]  flex-col justify-between   max-lg:w-[60px] max-lg:px-2  max-sm:hidden overflow-y-auto">
+    <div className="sticky top-0 flex h-dvh w-full max-w-[320px]  flex-col justify-between pt-24   max-lg:w-[60px] max-lg:px-2  max-sm:hidden overflow-y-auto">
       <div className="flex flex-1 flex-col gap-4">
         <div className="flex items-center gap-3">
           {/** TODO: bunu Next image olarak degis */}
