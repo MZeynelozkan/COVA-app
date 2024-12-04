c3ee979b31f520b91f43537e8e8ddc970a18d20b
MZeynelozkan <zeyzey4340@gmail.com>
2024-12-04 11:08:22 +0300

    top-artists layout

--- app/(root)/top-artists/page.tsx
+++ app/(root)/top-artists/page.tsx
@@ -1,7 +1,28 @@
 import React from "react";
 
+import ArtistCard from "@/components/cards/ArtistCard";
+
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div className="flex size-full flex-col items-start gap-4 p-4">
+      <div className="space-y-3">
+        <h1 className="text-light_dark text-4xl font-extrabold leading-[45px] tracking-[-1px] max-md:text-xl">
+          Top Artists
+        </h1>
+        <p className="text-base leading-[24px] text-[#6B6B6B]">
+          Explore the most popular artists
+        </p>
+      </div>
+      <div className="grid size-full grid-cols-4 gap-x-3 gap-y-2  py-4 max-lg:grid-cols-3 max-lg:p-2 max-md:grid-cols-2 max-md:p-0 max-sm:grid-cols-1">
+        {Array.from({ length: 4 }).map((item, index) => (
+          <ArtistCard key={index} />
+        ))}
+      </div>
+      <h2 className="self-center font-extralight">
+        This Page Only Contains Top 4 Artists
+      </h2>
+    </div>
+  );
 };
 
 export default page;
---
+++ components/cards/ArtistCard.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import React from "react";
+
+import { Card, CardHeader, CardContent } from "../ui/card";
+
+const ArtistCard = () => {
+  return (
+    <Card className="flex  size-full max-w-[499px] flex-col gap-3  border-none pb-3 shadow-none">
+      <CardHeader className="relative h-0 w-full pb-[100%]">
+        {" "}
+        {/* Aspect Ratio */}
+        <Image
+          src="/assets/icons/img1.png"
+          alt="img"
+          layout="fill"
+          objectFit="cover"
+          className="rounded-[12px]"
+        />
+      </CardHeader>
+      <CardContent className="flex flex-col gap-1  p-0">
+        <p className="text-light_dark text-base font-bold leading-[24px]">
+          Creator Name
+        </p>
+        <p className="font-normal leading-[21px] text-[#6B6B6B] dark:text-white">
+          500 likes • 20 collections
+        </p>
+        <p className="font-normal leading-[21px] text-[#6B6B6B] dark:text-white">
+          Most popular: Abstract
+        </p>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default ArtistCard;
