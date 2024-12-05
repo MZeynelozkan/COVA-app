761fc4208becc0b7677c58fc884cb2906b1f8c70
MZeynelozkan <zeyzey4340@gmail.com>
2024-12-05 15:48:28 +0300

    upd

--- auth.ts
+++ auth.ts
@@ -2,7 +2,7 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 
-import { prisma } from "./prisma";
+import prisma from "./prisma";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
--- prisma.ts
+++ prisma.ts
@@ -1,4 +1,15 @@
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
+const prismaClientSingleton = () => {
+  return new PrismaClient().$extends(withAccelerate());
+};
 
-export const prisma = new PrismaClient().$extends(withAccelerate());
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+
+export default prisma;
