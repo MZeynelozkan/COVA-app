eb7a9c29eca25dc0eeae82b1d282735fd396d378
MZeynelozkan <zeyzey4340@gmail.com>
2024-12-11 21:18:42 +0300

    upd

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -10,8 +10,6 @@ const Home = async () => {
     type: "ART",
   });
 
-  console.log(collections);
-
   return (
     <div className="grid size-full grid-cols-3 place-items-start gap-x-8 p-4 max-2xl:grid-cols-2 max-md:gap-y-5 max-sm:grid-cols-2  max-[375px]:grid-cols-1">
       {collections.map((item, index) => (
