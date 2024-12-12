6aca1a272e05f06df5af04adceb988d74565ce45
MZeynelozkan <zeyzey4340@gmail.com>
2024-12-12 23:25:53 +0300

    upd

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 // eslint-disable-next-line camelcase, import/order
 import { Plus_Jakarta_Sans } from "next/font/google";
 
+// eslint-disable-next-line import/order
+import Head from "next/head"; // next/head bileşenini içe aktarın
+
 import "./globals.css";
 import "@/styles/theme.css";
 import { SessionProvider } from "next-auth/react";
@@ -26,6 +29,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        {/* Google AdSense Script */}
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4289366483169589"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
       <body className={plusJakartaSans.className}>
         <ThemeProvider
           attribute="class"
