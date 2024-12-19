54042eaf6a4c0b440f45721b303c284108f99a6d
MZeynelozkan <zeyzey4340@gmail.com>
2024-12-19 21:18:52 +0300

    upd

--- app/(root)/top-artists/page.tsx
+++ app/(root)/top-artists/page.tsx
@@ -3,7 +3,24 @@ import React from "react";
 import ArtistCard from "@/components/cards/ArtistCard";
 import { getTopArtists } from "@/lib/actions/user.action";
 
-const page = async () => {
+export async function generateMetadata() {
+  const topArtists = await getTopArtists();
+
+  return {
+    title: "Top Artists",
+    description: "Explore the most popular artists",
+    keywords: topArtists.map((artist) => artist.name).join(", "),
+    openGraph: {
+      title: "Top Artists",
+      description: "Explore the most popular artists",
+      images: topArtists.map(
+        (artist) => artist.image || "https://via.placeholder.com/300"
+      ), // Fallback image
+    },
+  };
+}
+
+const Page = async () => {
   const topArtists = await getTopArtists();
 
   return (
@@ -16,7 +33,7 @@ const page = async () => {
           Explore the most popular artists
         </p>
       </div>
-      <div className="grid size-full grid-cols-4 gap-x-3 gap-y-2  py-4 max-lg:grid-cols-3 max-lg:p-2 max-md:grid-cols-2 max-md:p-0 max-sm:grid-cols-1">
+      <div className="grid size-full grid-cols-4 gap-x-3 gap-y-2 py-4 max-lg:grid-cols-3 max-lg:p-2 max-md:grid-cols-2 max-md:p-0 max-sm:grid-cols-1">
         {topArtists.map((item, index) => (
           <ArtistCard
             key={index}
@@ -38,4 +55,4 @@ const page = async () => {
   );
 };
 
-export default page;
+export default Page;
