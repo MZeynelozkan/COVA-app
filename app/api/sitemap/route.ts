1b80d0cd400b867ae64e853e2de7eba4f6a7423c
MZeynelozkan <zeyzey4340@gmail.com>
2024-12-21 13:07:33 +0300

    upd

--- app/api/sitemap/route.ts
+++ app/api/sitemap/route.ts
@@ -1,7 +1,9 @@
 import { NextResponse } from "next/server";
 
+import { prisma } from "@/prisma";
+
 export async function GET() {
-  const baseUrl = "https://www.cova-app.com.tr"; // Kendi sitenizin URL'sini yazın.
+  const baseUrl = "https://www.cova-app.com.tr"; // Sitenizin URL'sini yazın.
 
   // Statik sayfa yolları
   const staticPaths = [
@@ -18,6 +20,15 @@ export async function GET() {
     "/video",
   ];
 
+  // Dinamik verileri al
+  const collections = await prisma.collection.findMany({
+    select: { id: true },
+  });
+
+  const profiles = await prisma.user.findMany({
+    select: { id: true, updatedAt: true },
+  });
+
   // Site haritası XML'i oluşturuluyor
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
@@ -28,6 +39,27 @@ export async function GET() {
       <loc>${baseUrl}${path}</loc>
       <changefreq>weekly</changefreq>
       <priority>0.8</priority>
+    </url>`;
+    })
+    .join("")}
+  ${collections
+    .map((collection) => {
+      return `
+    <url>
+      <loc>${baseUrl}/collection/${collection.id}</loc>
+      <changefreq>weekly</changefreq>
+      <priority>0.7</priority>
+    </url>`;
+    })
+    .join("")}
+  ${profiles
+    .map((profile) => {
+      return `
+    <url>
+      <loc>${baseUrl}/profile/${profile.id}</loc>
+      <lastmod>${profile.updatedAt.toISOString()}</lastmod>
+      <changefreq>weekly</changefreq>
+      <priority>0.6</priority>
     </url>`;
     })
     .join("")}
